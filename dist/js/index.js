0cd6fd2c5429b0c10bf01847be8112f98df3f722
Hong Baojin <13809735212@163.com>
2019-07-29 23:10:37 +0800

    basically completed

--- dist/js/index.js
+++ dist/js/index.js
@@ -17,25 +17,25 @@ $(function () {
     $this.parent().parent().find('.qlist').eq($this.index()).addClass('qlistshow');
   });
 
-  // 头部固定
-  var headerTop = $('.header2').offset().top; // 头部距离顶部距离
-  window.addEventListener("scroll", function (e) {
-    // 滚动的距离
-    var rollTop = document.documentElement.scrollTop || document.body.scrollTop;
-    var distance = headerTop - rollTop; // 距离差
-    if (distance < 0) {
+  // header 悬浮置顶
+  var headerTop = $('.header2').offset().top;
+  var headerSite = '<div class="header-site"></div>';
+  $('.wrap').prepend(headerSite);
+
+  $(window).scroll(function () {
+    if (headerTop < $(this).scrollTop()) {
       $('.header2').css({
-        "width": "100%",
+        "width": "10rem",
         "backgroundColor": "#ffffff",
         "zIndex": "20",
         "position": "fixed",
-        "boxShadow": "0 1px 3px rgba(0, 0, 0, 0.2)"
+        'top': '0',
+        "boxShadow": "0 0 3px rgba(0, 0, 0, 0.2)"
       });
+      $('.header-site').css('height', '1.17333rem').show();
     } else {
-      $('.header2').css({
-        "position": "relative",
-        "boxShadow": "none"
-      });
+      $('.header2').css('position', 'relative');
+      $('.header-site').hide();
     }
   });
 });
