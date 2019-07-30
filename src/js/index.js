f02d3f3c166102820540ec3187e7d215860ed40f
Hong Baojin <13809735212@163.com>
2019-07-30 11:15:16 +0800

    accomplish

--- src/js/index.js
+++ src/js/index.js
@@ -15,27 +15,14 @@ $(function () {
     $this.parent().parent().find('.qlist').eq($this.index()).addClass('qlistshow');
   });
 
-  // header 悬浮置顶
-  var headerTop = $('.header2').offset().top;
-  var headerSite = '<div class="header-site"></div>';
-  $('.wrap').prepend(headerSite);
-
+  //头部悬浮
   $(window).scroll(function () {
-    if (headerTop < $(this).scrollTop()) {
-      $('.header2').css(
-        {
-          "width": "10rem",
-          "backgroundColor": "#ffffff",
-          "zIndex": "20",
-          "position": "fixed",
-          'top': '0',
-          "boxShadow": "0 0 3px rgba(0, 0, 0, 0.2)"
-        }
-      );
-      $('.header-site').css('height', '1.17333rem').show();
-    } else {
-      $('.header2').css('position', 'relative');
-      $('.header-site').hide();
+    if ($('.quesw').size() > 0) {
+      if ($(this).scrollTop() > $('.quesw').height() + $('.header2').height()) {
+        $('.quesw').addClass('hf')
+      } else {
+        $('.quesw').removeClass('hf')
+      }
     }
   });
 });
